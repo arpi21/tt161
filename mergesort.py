8794a6588b4a5eb4c26b4447928ffd13de6746af
arpi21 <64080709+arpi21@users.noreply.github.com>
2023-10-24 22:15:27 +0400

    Merge Sort

---
+++ mergesort.py
@@ -0,0 +1,33 @@
+def merge(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left= arr[:mid]
+        right= arr[mid:]
+
+        merge(left)
+        merge(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+arr = [47,2,3,4,0,9,41]
+merge(arr)
+print(arr)
