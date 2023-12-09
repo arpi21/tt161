70408b246fcfcdc9332656680d862913587903ec
arpi21 <64080709+arpi21@users.noreply.github.com>
2023-12-09 20:28:34 +0400

    Create rotated_sorted_array.py

---
+++ rotated_sorted_array.py
@@ -0,0 +1,26 @@
+def search_rotated(num, target):
+    start = 0
+    end = len(nums) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        if nums[start] <= nums[mid]:
+            if nums[start] <= target < nums[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        else: 
+            if nums[mid] < target <= nums[end]:
+                start = mid + 1
+            else:
+                end = mid - 1
+
+    return -1
+
+nums = [4, 5, 6, 7, 0, 1, 2]
+target = 0
+print(search_rotated(nums, target))
